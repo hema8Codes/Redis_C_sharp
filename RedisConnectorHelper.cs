4430921552c2993313a74e593752ad58c2096148
Hemakshi Pandey <developer8hema@gmail.com>
2023-04-11 12:51:33 +0530

    Intro to Redis Connection C#

---
+++ RedisConnectorHelper.cs
@@ -0,0 +1,59 @@
+﻿using StackExchange.Redis;
+
+public class CacheConnection
+{
+    private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+    {
+
+
+        var options = new ConfigurationOptions
+        {
+            EndPoints = { { "127.0.0.1", 6379 } },
+            ConnectTimeout = 10000,
+            SyncTimeout = 10000,
+            AbortOnConnectFail = false
+        };
+
+        return ConnectionMultiplexer.Connect(options);
+
+
+    }, System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+    public static ConnectionMultiplexer Connection
+    {
+        get
+        {
+            return lazyConnection.Value;
+        }
+    }
+}
+
+public class RedisConnectorHelper
+{
+
+
+    static RedisConnectorHelper()
+    {
+        var options = new ConfigurationOptions
+        {
+            EndPoints = { { "127.0.0.1", 6379 } },
+            ConnectTimeout = 10000,
+            SyncTimeout = 10000,
+            AbortOnConnectFail = false
+        };
+        RedisConnectorHelper.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            return ConnectionMultiplexer.Connect(options);
+        });
+    }
+
+    private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+    public static ConnectionMultiplexer Connection
+    {
+        get
+        {
+            return lazyConnection.Value;
+        }
+    }
+}
